7ec8f6ad0e3688c6060ecc5e9965144ffcb83c70
Gilles Felix <felix@novius.fr>
2025-06-17 09:56:16 +0200

    Add loadMigration

--- src/RedirectManagerServiceProvider.php
+++ src/RedirectManagerServiceProvider.php
@@ -3,33 +3,17 @@
 namespace Novius\LaravelFilamentRedirectManager;
 
 use Illuminate\Support\ServiceProvider;
-use Spatie\LaravelPackageTools\Package;
 
 class RedirectManagerServiceProvider extends ServiceProvider
 {
-    public static string $name = 'laravel-filament-redirect-manager';
-
-    public function configurePackage(Package $package): void
-    {
-        $package->name('laravel-filament-redirect-manager')
-            ->hasRoute('web')
-            ->hasConfigFile('laravel-filament-redirect-manager')
-            ->hasTranslations()
-            ->hasMigration('create_redirects_table');
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         $packageDir = dirname(__DIR__);
 
         $this->publishes([$packageDir.'/config' => config_path()], 'config');
 
-        $this->publishes([
-            $packageDir.'/database/migrations' => database_path('migrations'),
-        ], 'migrations');
+        $this->publishes([$packageDir.'/database/migrations' => database_path('migrations')], 'migrations');
+        $this->loadMigrationsFrom($packageDir.'/database/migrations');
 
         $this->loadTranslationsFrom($packageDir.'/lang', 'laravel-filament-redirect-manager');
         $this->publishes([__DIR__.'/../lang' => resource_path('../lang/vendor/laravel-filament-redirect-manager')], 'lang');
