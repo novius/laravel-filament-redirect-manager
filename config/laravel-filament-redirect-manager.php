9f1e1131154a8450a0de476124554b5d9ad6969b
Gilles Felix <felix@novius.fr>
2026-05-12 08:56:47 +0200

    Add Laravel 13 compatiility

--- config/laravel-filament-redirect-manager.php
+++ config/laravel-filament-redirect-manager.php
@@ -1,14 +1,18 @@
 <?php
 
+use Novius\LaravelFilamentRedirectManager\Filament\Redirects\RedirectResource;
+use Novius\LaravelFilamentRedirectManager\Models\Redirect;
+use Novius\LaravelFilamentRedirectManager\Redirector\DatabaseRedirector;
+
 return [
 
-    'redirector_model' => \Novius\LaravelFilamentRedirectManager\Models\Redirect::class,
+    'redirector_model' => Redirect::class,
 
     'redirect_url_max_length' => 1000,
 
-    'redirector' => \Novius\LaravelFilamentRedirectManager\Redirector\DatabaseRedirector::class,
+    'redirector' => DatabaseRedirector::class,
 
     'resources' => [
-        'RedirectResource' => \Novius\LaravelFilamentRedirectManager\Filament\Redirects\RedirectResource::class,
+        'RedirectResource' => RedirectResource::class,
     ],
 ];
